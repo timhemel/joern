6f3d6a10346eddb3758c13930b70515b3106505f
Tim Hemel <tim@securesoftware.nl>
2017-09-28 15:44:05 +0200

    Create JoernTransferFunction objects dynamically based on "type" property.

--- projects/octopus/src/main/java/octopus/analysis/joern/JoernTransferFunctionFactory.java
+++ projects/octopus/src/main/java/octopus/analysis/joern/JoernTransferFunctionFactory.java
@@ -9,16 +9,28 @@ import java.util.stream.Stream;
 
 import octopus.analysis.joern.*;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class JoernTransferFunctionFactory {
 
-	private static final Map<String,Class<?>> xferMap = Stream.of(
-		new SimpleEntry<String,Class<?>>("Condition",JoernTransferFunctionCondition.class),
-		new SimpleEntry<String,Class<?>>("CFGEntryNode",JoernTransferFunctionCFGEntryNode.class),
-		new SimpleEntry<String,Class<?>>("CFGExitNode",JoernTransferFunctionCFGExitNode.class)
-	).collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
-
 	public static JoernTransferFunction create(Vertex v) {
-		return new JoernTransferFunction(v);
+		try {
+			Class<? extends JoernTransferFunction> klass = (Class<? extends JoernTransferFunction>) Class.forName("octopus.analysis.joern.JoernTransferFunction"+v.value("type"));
+			Constructor<? extends JoernTransferFunction> ctor = klass.getConstructor(Vertex.class);
+			JoernTransferFunction xfer = ctor.newInstance(v);
+			return xfer;
+		} catch (ClassNotFoundException e) {
+			return null;
+		} catch (NoSuchMethodException e) {
+			return null;
+		} catch (InstantiationException e) {
+			return null;
+		} catch (IllegalAccessException e) {
+			return null;
+		} catch (InvocationTargetException e) {
+			return null;
+		}
 	}
 
 }
